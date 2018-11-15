eb06af380920ed1680ba3276aa695e601f4b4cbc
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-15 21:03:29 +0100

    fix errors

---
+++ src/main/scala/com/inland24/housingml/AppConfig.scala
@@ -0,0 +1,23 @@
+package com.inland24.housingml
+
+import com.typesafe.config.Config
+
+import scala.util.Try
+
+final case class AppConfig(
+  environment: String,
+  sourceFileName: String,
+  sourceFileUrl: String,
+  targetFilePath: String
+)
+object AppConfig {
+
+  def load(cfg: Config): Try[AppConfig] = Try {
+    AppConfig(
+      environment = cfg.getString("environment"),
+      sourceFileName = cfg.getString("file.name"),
+      sourceFileUrl = cfg.getString("file.from.url"),
+      targetFilePath = cfg.getString("file.to.path")
+    )
+  }
+}
--- src/main/scala/com/inland24/housingml/Main.scala
+++ src/main/scala/com/inland24/housingml/Main.scala
@@ -6,35 +6,27 @@ import java.net.URL
 import better.files._
 
 import scala.language.postfixOps
-import com.typesafe.config.{ConfigFactory, ConfigParseOptions, ConfigResolveOptions}
 import org.apache.commons.io.FileUtils
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Option(System.getProperty("env", "")) match {
-      case Some(envName) =>
-        val config = ConfigFactory.load(
-          s"application.${envName.toLowerCase}.conf",
-          ConfigParseOptions.defaults().setAllowMissing(false),
-          ConfigResolveOptions.defaults()
-        ).resolve()
-        val fileName = config.getString("file.name")
-        val downloadURL = config.getString("file.from.url")
-        val localDirPath = config.getString("file.to.path")
 
+    // 1. Load the configuration
+    AppConfig.load(ConfigUtil.loadFromEnv()) match {
+      case Success(appCfg) =>
         // 0. Check if we have the target dir's created, if not create them
-        val localDir = File(localDirPath).createDirectories()
+        val localDir = File(appCfg.targetFilePath).createDirectories()
 
         val result = for {
           // 1. Download the file and store it locally
-          _ <- download(new URL(downloadURL), new JFile(localDir.toJava, fileName))
+          _ <- download(new URL(appCfg.sourceFileUrl), new JFile(localDir.toJava, appCfg.sourceFileName))
           // 2. Unzip the contents
-          _ <- unzip(File(s"${localDir.path}/$fileName"), File(s"${localDir.path}/housing.csv"))
+          _ <- unzip(File(s"${localDir.path}/${appCfg.sourceFileName}"), File(s"${localDir.path}/housing.csv"))
         } yield {
           println("Successfully ran the file pre-processing")
         }
@@ -44,8 +36,9 @@ object Main {
             println(s"Some stupidity happened ${ex.getMessage}")
             System.exit(-1)
         }
-      case None =>
-        println("No environment variable setting found! So exiting run...")
+      case Failure(ex) =>
+        println(s"No environment variable setting found! So exiting run...." +
+          s"failed with the error message ${ex.getMessage}")
         println("Usage: sbt -Denv=test run")
         System.exit(-1)
     }
